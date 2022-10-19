77b8a572124b66cd3a9660256aca1de638ab9ff1
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-10-19 19:38:38 +0700

    mengintegrasikan project dengan firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lokerify/presentation/pages/splash_page.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
